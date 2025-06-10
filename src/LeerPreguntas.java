bda487e6ee7d7bf133fc2230e996158e4297a467
JuanDiegoLopezMorales <lopezjuandiego411@gmail.com>
2025-06-09 22:07:24 -0500

    Update LeerPreguntas.java

--- src/LeerPreguntas.java
+++ src/LeerPreguntas.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+public class LeerPreguntas implements Runnable {
 
-public class LeerPreguntas {
+    @Override
+    public void run() {
+        System.out.println("leyendo las preguntas de la tarea");
+        try{
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Se le dio una primera vista a las preguntas");
+
+    }
 }
