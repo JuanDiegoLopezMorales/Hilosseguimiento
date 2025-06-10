bd06d1b2475429cafe2f373005d2e9687a394973
JuanDiegoLopezMorales <lopezjuandiego411@gmail.com>
2025-06-09 22:07:09 -0500

    Update HacerTarea.java

--- src/HacerTarea.java
+++ src/HacerTarea.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+public class HacerTarea implements Runnable{
+    @Override
+    public void run() {
 
-public class HacerTarea {
+        System.out.println("Abriendo chat gpt, claude ai y Arle pdf de explicación");
+        try{
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("La tarea se termino con ayuda de el pdf de Arle");
+    }
+
+
 }
