a6e43f8d77d1966d47a45983d1d2c0e6321ae503
JuanDiegoLopezMorales <lopezjuandiego411@gmail.com>
2025-06-09 22:06:53 -0500

    Update EntregarTarea.java

--- src/EntregarTarea.java
+++ src/EntregarTarea.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+public class EntregarTarea implements Runnable{
+    @Override
+    public void run() {
 
-public class EntregarTarea {
+        System.out.println("Abriendo portal cue");
+        try {
+            Thread.sleep(15000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Se envio el codigo para que Arle no lo termine revisando");
+    }
+
 }
