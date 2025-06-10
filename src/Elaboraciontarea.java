6df1dd76aa8a23c31ef029a74697c60916313e13
JuanDiegoLopezMorales <lopezjuandiego411@gmail.com>
2025-06-09 22:06:34 -0500

    Update Elaboraciontarea.java

--- src/Elaboraciontarea.java
+++ src/Elaboraciontarea.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class Elaboraciontarea {
+    public static void main(String[] args) {
+        Thread leerPreguntas = new Thread(new LeerPreguntas());
+        Thread hacerTarea = new Thread(new HacerTarea());
+        Thread entregarTarea = new Thread(new EntregarTarea());
+
+        leerPreguntas.start();
+        hacerTarea.start();
+        entregarTarea.start();
+
+    }
 }
